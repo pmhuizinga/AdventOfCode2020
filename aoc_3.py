38b5d03e95202d790a3a8c1a382b51b903ab55d8
pmhuizinga <pmhuizinga@hotmail.com>
2020-12-11 18:52:07 +0100

    advent of code day 3: answer is correct

--- aoc_3.py
+++ aoc_3.py
@@ -7,8 +7,11 @@ import numpy as np
 import math
 
 # vars
-right = 3
-down = 1
+
+# right = 3
+# down = 1
+slopes_test = [[1, 1], [3, 1]]
+slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
 
 # read
 aoc = r'aoc3.txt'
@@ -21,20 +24,26 @@ data = np.array([[b.replace('.', '0').replace('#', '1') for b in a.strip()] for 
 
 # the array needs to duplicated x times ('right' value times the number of rows)
 nr_of_rows = len(data)
-required_nr_of_columns = right * nr_of_rows
-nr_of_duplications = math.ceil(required_nr_of_columns / data.shape[1])
-d = data
-for i in range(nr_of_duplications):
-    d = np.concatenate([d, data], axis=1)
 
-# loop through the array and sum all the ones
-row_pos = 0
-col_pos = 0
-val = 0
-for idx, row in enumerate(range(d.shape[0]-1)):
-    col_pos = col_pos + right
-    row_pos = row_pos + down
-    val = val + int(d[row_pos][col_pos])
-    print(row_pos, col_pos)
+def calc_trees(right, down):
+    required_nr_of_columns = right * nr_of_rows
+    nr_of_duplications = math.ceil(required_nr_of_columns / data.shape[1])
+    d = data
+    for i in range(nr_of_duplications):
+        d = np.concatenate([d, data], axis=1)
 
-print(val)
+    # loop through the array and sum all the ones
+    row_pos = 0
+    col_pos = 0
+    val = 0
+    for idx, row in enumerate(range(d.shape[0] - 1)):
+        col_pos = col_pos + right
+        row_pos = row_pos + down
+        val = val + int(d[row_pos][col_pos])
+        print(row_pos, col_pos)
+
+    return val
+
+[x[0] for x in slopes_test]
+
+
