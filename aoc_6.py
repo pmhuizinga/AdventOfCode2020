7894cf693dc446cb88a21f0dcbfa6e15931974d4
pmhuizinga <pmhuizinga@hotmail.com>
2020-12-15 17:00:53 +0100

    advent of code day 6

--- aoc_6.py
+++ aoc_6.py
@@ -40,22 +40,64 @@ In this example, the sum of these counts is 3 + 3 + 3 + 1 + 1 = 11.
 For each group, count the number of questions to which anyone answered "yes". What is the sum of those counts?
 """
 # read
-with open(r'data/aoc6.txt', 'r') as f:
+with open(r'data/aoc6_test.txt', 'r') as f:
     data = f.read()
 
 data2 = data.split('\n')
 # append emtpy item for use in loop
-data2.append('')
+# data2.append('')
 
 # make list, split by blank records
 d = []
+e = []
 y = ''
+z = []
 for x in data2:
     if x == '':
         d.append(y)
+        e.append(z)
         y = ''
+        z = []
     else:
         y = str(x) + str(y)
+        z.append(str(x))
 
 total = sum([len(set(x)) for x in d])
 print('the sum of counts is {}'.format(total))
+
+"""
+--- Part Two ---
+As you finish the last group's customs declaration, you notice that you misread one word in the instructions:
+
+You don't need to identify the questions to which anyone answered "yes"; you need to identify the questions to 
+which everyone answered "yes"!
+
+Using the same example as above:
+
+abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
+This list represents answers from five groups:
+
+In the first group, everyone (all 1 person) answered "yes" to 3 questions: a, b, and c.
+In the second group, there is no question to which everyone answered "yes".
+In the third group, everyone answered yes to only 1 question, a. Since some people did not answer "yes" to b or c, 
+they don't count.
+In the fourth group, everyone answered yes to only 1 question, a.
+In the fifth group, everyone (all 1 person) answered "yes" to 1 question, b.
+In this example, the sum of these counts is 3 + 0 + 1 + 1 + 1 = 6.
+
+For each group, count the number of questions to which everyone answered "yes". What is the sum of those counts?
+"""
