2d60774c6068774140e9b13ddfd9fcf0e7a38150
pmhuizinga <pmhuizinga@hotmail.com>
2020-12-22 00:51:21 +0100

    advent of code day 7

--- aoc_7.py
+++ aoc_7.py
@@ -30,7 +30,8 @@ In the above rules, the following options would be available to you:
 A bright white bag, which can hold your shiny gold bag directly.
 A muted yellow bag, which can hold your shiny gold bag directly, plus some other bags.
 A dark orange bag, which can hold bright white and muted yellow bags, either of which could then hold your shiny
-gold bag. A light red bag, which can hold bright white and muted yellow bags, either of which could then hold your
+gold bag.
+A light red bag, which can hold bright white and muted yellow bags, either of which could then hold your
 shiny gold bag. So, in this example, the number of bag colors that can eventually contain at least one shiny
 gold bag is 4.
 
@@ -43,23 +44,73 @@ make sure you get all of it.)
 # the number of bags is not relevant
 # create matrix
 
+import numpy as np
+import pandas as pd
+
 # read
 with open(r'data/aoc7_test.txt', 'r') as f:
     data = f.read()
 
 data2 = [[y.replace('bags', '').replace('bag', '').replace('no other', '0 no other').replace('.', '').strip() for y in x.split('contain')] for x in
          data.split('\n')]
-data2
 
-# import re
-#
-# temp = re.compile("([a-zA-Z]+)([0-9]+)")
-# res = temp.match('3 clear teal , 2 dull tomato , 5 dotted purple , 1 posh salmon').groups()
+# make new list
+new_list = []
+for rule in data2:
+    tmp_list = [rule[0]]
+    content = rule[1].split(',')
+    f = []
+    for item in content:
+        d = item.split()
+        string = ''
+        g = []
+        for e in d:
+            if e.isdigit():
+                g.append(int(e))
+            else:
+                string = string + ' ' + e
+        g.append(string.strip())
+        f.append(g)
+    tmp_list.append(f)
+    new_list.append(tmp_list)
 
 
-# data3 = [x[1] for x in data2]
+# # get all options
+# options = []
+# for x in new_list:
+#     options.append(x[0])
+#     for y in x[1]:
+#         options.append(y[1])
 #
+# options = list(set(options))
+# options.sort()
+# nr_of_options = len(options)
 #
-# for x in data2:
-#     x[1] = x[1].split(',')
-#     print(x)
+# # make array
+# array = np.zeros((nr_of_options, nr_of_options))
+# # make dataframe
+# df = pd.DataFrame(array, columns=options)
+# df['index'] = options
+# df.set_index(['index'], inplace=True)
+
+# fill dataframe
+list2 = []
+for x in new_list:
+    for y in x[1]:
+        l = [x[0], y[1], y[0]]
+        list2.append(l)
+
+def recursive_shizzle(list, target_item_to_find, counter=0):
+    print('-' * 60)
+    print('target {}, çounter {}'.format(target_item_to_find, counter))
+    for combination in list:
+        # print(combination)
+        if combination[1] == target_item_to_find:
+            counter = recursive_shizzle(list, combination[0], counter + 1)
+            print('combination[0] {}'.format(combination[0]))
+
+    return counter
+
+# testlist = [['a', 'b'],['c', 'a'], ['d', 'b'],['z', 'c']]
+result = recursive_shizzle(list2, 'shiny gold')
+result
