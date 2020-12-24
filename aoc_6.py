4c88eb94c18ed7fba883c4ba70655eb879ee907d
pmhuizinga <pmhuizinga@hotmail.com>
2020-12-24 10:10:53 +0100

    moved code into functions

--- aoc_6.py
+++ aoc_6.py
@@ -1,40 +1,54 @@
 # read
-with open(r'data/aoc6.txt', 'r') as f:
-    data = f.read()
+def prep_data(filepath):
 
-data2 = data.split('\n')
-# append emtpy item for use in loop
-data2.append('')
+    with open(filepath, 'r') as f:
+        data = f.read()
 
-# make list, split by blank records
-d = []
-e = []
-y = ''
-z = []
-for x in data2:
-    if x == '':
-        d.append(y)
-        e.append(z)
-        y = ''
-        z = []
-    else:
-        y = str(x) + str(y)
-        z.append(str(x))
+    data2 = data.split('\n')
+    # append emtpy item for use in loop
+    data2.append('')
 
-total = sum([len(set(x)) for x in d])
-print('the sum of counts is {}'.format(total))
-print('-' * 30)
+    # make list, split by blank records
+    d = []
+    e = []
+    y = ''
+    z = []
+    for x in data2:
+        if x == '':
+            d.append(y)
+            e.append(z)
+            y = ''
+            z = []
+        else:
+            y = str(x) + str(y)
+            z.append(str(x))
+
+    return d, e
+
+def day6a(data):
+
+    total = sum([len(set(x)) for x in data[0]])
+
+    return total
+
+result = day6a(prep_data(r'data/aoc6.txt'))
+
+print('result of day 6a is {}'.format(result))
 
 """
 part 2
 """
 
-counter = 0
-for x in e:
-    z = set(x[0])
-    for y in range(len(x)):
-        z = set(x[y]).intersection(z)
-    counter += len(z)
+def day6b(data):
+    counter = 0
+    for x in data[1]:
+        z = set(x[0])
+        for y in range(len(x)):
+            z = set(x[y]).intersection(z)
+        counter += len(z)
 
-print('counter is {}'.format(counter))
+    return counter
+
+result = day6b(prep_data(r'data/aoc6.txt'))
+print('result of day 6b is {}'.format(result))
 
--- aoc_7.py
+++ aoc_7.py
@@ -1,84 +1,40 @@
-"""
---- Day 7: Handy Haversacks ---
-You land at the regional airport in time for your next flight. In fact, it looks like you'll even have time to
-grab some food: all flights are currently delayed due to issues in luggage processing.
+def prep_data(filepath):
 
-Due to recent aviation regulations, many rules (your puzzle input) are being enforced about bags and their contents;
-bags must be color-coded and must contain specific quantities of other color-coded bags. Apparently, nobody
-responsible for these regulations considered how long they would take to enforce!
+    with open(filepath, 'r') as f:
+        data = f.read()
 
-For example, consider the following rules:
+    data2 = [[y.replace('bags', '').replace('bag', '').replace('no other', '0 no other').replace('.', '').strip() for y in
+              x.split('contain')] for x in
+             data.split('\n')]
 
-light red bags contain 1 bright white bag, 2 muted yellow bags.
-dark orange bags contain 3 bright white bags, 4 muted yellow bags.
-bright white bags contain 1 shiny gold bag.
-muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
-shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
-dark olive bags contain 3 faded blue bags, 4 dotted black bags.
-vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
-faded blue bags contain no other bags.
-dotted black bags contain no other bags.
-These rules specify the required contents for 9 bag types. In this example, every faded blue bag is empty, every
-vibrant plum bag contains 11 bags (5 faded blue and 6 dotted black), and so on.
+    # make list of list of all source bags and all the bags it contains
+    new_list = []
+    for rule in data2:
+        tmp_list = [rule[0]]
+        content = rule[1].split(',')
+        f = []
+        for item in content:
+            d = item.split()
+            string = ''
+            g = []
+            for e in d:
+                if e.isdigit():
+                    g.append(int(e))
+                else:
+                    string = string + ' ' + e
+            g.append(string.strip())
+            f.append(g)
+        tmp_list.append(f)
+        new_list.append(tmp_list)
 
-You have a shiny gold bag. If you wanted to carry it in at least one other bag, how many different bag colors
-would be valid for the outermost bag? (In other words: how many colors can, eventually, contain at least one
-shiny gold bag?)
+    # convert to a 1 on 1 relation including the number of bags
+    list2 = []
+    for x in new_list:
+        for y in x[1]:
+            l = [x[0], y[1], y[0]]
+            list2.append(l)
 
-In the above rules, the following options would be available to you:
-
-A bright white bag, which can hold your shiny gold bag directly.
-A muted yellow bag, which can hold your shiny gold bag directly, plus some other bags.
-A dark orange bag, which can hold bright white and muted yellow bags, either of which could then hold your shiny
-gold bag.
-A light red bag, which can hold bright white and muted yellow bags, either of which could then hold your
-shiny gold bag. So, in this example, the number of bag colors that can eventually contain at least one shiny
-gold bag is 4.
-
-How many bag colors can eventually contain at least one shiny gold bag? (The list of rules is quite long;
-make sure you get all of it.)
-"""
-import pytest
-
-# read
-import os
-print(os.getcwd())
-
-
-with open(r'data/aoc7.txt', 'r') as f:
-    data = f.read()
-
-data2 = [[y.replace('bags', '').replace('bag', '').replace('no other', '0 no other').replace('.', '').strip() for y in
-          x.split('contain')] for x in
-         data.split('\n')]
-
-
-# make list of list of all source bags and all the bags it contains
-new_list = []
-for rule in data2:
-    tmp_list = [rule[0]]
-    content = rule[1].split(',')
-    f = []
-    for item in content:
-        d = item.split()
-        string = ''
-        g = []
-        for e in d:
-            if e.isdigit():
-                g.append(int(e))
-            else:
-                string = string + ' ' + e
-        g.append(string.strip())
-        f.append(g)
-    tmp_list.append(f)
-    new_list.append(tmp_list)
-
-# convert to a 1 on 1 relation including the number of bags
-list2 = []
-for x in new_list:
-    for y in x[1]:
-        l = [x[0], y[1], y[0]]
-        list2.append(l)
+    return list2
 
 # recursive function for looping through the list
 # counts every unique relation
@@ -95,19 +51,9 @@ def day7a(list, target_item_to_find, list_of_knowns=None, counter=0):
 
     return counter
 
-result = day7a(list2, 'shiny gold')
-print(result)
-
-
-def test_day7a():
-    testlist = [['bright white', 'shiny gold'],
-                ['muted yellow', 'shiny gold'],
-                ['light red', 'bright white'],
-                ['dark orange', 'bright white'],
-                ['light red', 'muted yellow'],
-                ['dark orange', 'muted yellow']]
-
-    assert day7a(testlist, 'shiny gold') == 4
+a = r'data/aoc7.txt'
+result = day7a(prep_data(a), 'shiny gold')
+print('result of day 7a is: {}'.format(result))
 
 """
 part 2
@@ -122,15 +68,6 @@ testlist = [['shiny gold white', 'dark red', 2],
             ['dark violet', 'no other', 0]]
 
 def day7b():
+
     return 4
 
-def test_day7b():
-    testlist = [['shiny gold white', 'dark red', 2],
-                ['dark red', 'dark orange', 2],
-                ['dark orange', 'dark yellow', 2],
-                ['dark yellow', 'dark green', 2],
-                ['dark green', 'dark blue', 2],
-                ['dark blue', 'dark violet', 2],
-                ['dark violet', 'no other', 0]]
-
-    assert day7a(testlist, 'shiny gold') == 4
