d93c1512d5d2334ec5ece538797194441f4e927a
pmhuizinga <pmhuizinga@hotmail.com>
2020-12-27 10:50:53 +0100

    aoc day 9

--- aoc_9.py
+++ aoc_9.py
@@ -7,14 +7,11 @@ def prep_data(filepath):
 
     return data
 
-
 d = prep_data(r'data/aoc9.txt')
-
 
 def find_values(data, summed_value):
     for x in data:
         if summed_value - x in data:
-            # y = summed_value - x
             return True
 
     return False
@@ -30,20 +27,19 @@ def day9a(data, preamble=25):
             return data[i]
 
 
-print(day9a(d))
-
 def day9b(data):
+
     invalid_number = day9a(d)
-    # 69316178
+
     # 1 find position in list
     position_of_invalid_number = ([i for i, x in enumerate(data) if x == invalid_number])[0]
-    # print(position_of_invalid_number)
 
     for i in range(0, position_of_invalid_number):
         for j in range(i + 1, position_of_invalid_number):
             if sum(data[i:j]) == invalid_number:
-                # print(data[i:j])
                 return min(data[i:j]) + max(data[i:j])
                 break
 
+
+print(day9a(d))
 print(day9b(d))
