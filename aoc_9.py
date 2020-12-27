54d51220a22e7a756c144e56c242601c1417c706
pmhuizinga <pmhuizinga@hotmail.com>
2020-12-27 10:48:55 +0100

    aoc day 9

--- aoc_9.py
+++ aoc_9.py
@@ -3,11 +3,47 @@ def prep_data(filepath):
         data = f.read()
 
     data = data.split()
+    data = [int(x) for x in data]
+
     return data
 
-d = prep_data(r'test/aoc9.txt')
 
-def aoc9a(data, preamble=5):
+d = prep_data(r'data/aoc9.txt')
+
+
+def find_values(data, summed_value):
+    for x in data:
+        if summed_value - x in data:
+            # y = summed_value - x
+            return True
+
+    return False
+
+
+def day9a(data, preamble=25):
     length = len(data)
 
+    for i in range(preamble, length):
 
+        inputlist = data[i - preamble:i]
+        if find_values(inputlist, data[i]) == False:
+            return data[i]
+
+
+print(day9a(d))
+
+def day9b(data):
+    invalid_number = day9a(d)
+    # 69316178
+    # 1 find position in list
+    position_of_invalid_number = ([i for i, x in enumerate(data) if x == invalid_number])[0]
+    # print(position_of_invalid_number)
+
+    for i in range(0, position_of_invalid_number):
+        for j in range(i + 1, position_of_invalid_number):
+            if sum(data[i:j]) == invalid_number:
+                # print(data[i:j])
+                return min(data[i:j]) + max(data[i:j])
+                break
+
+print(day9b(d))
--- test/test_aoc.py
+++ test/test_aoc.py
@@ -3,6 +3,7 @@ os.chdir('../')
 
 import aoc_7
 import aoc_8
+import aoc_9
 
 class Test_AdventOfCode():
 
@@ -25,3 +26,13 @@ class Test_AdventOfCode():
     def test_day8b(self):
 
         assert aoc_8.day8b(aoc_8.prep_data(r'test/aoc8.txt')) == 8
+
+
+    def test_day9a(self):
+
+        assert aoc_9.day9a(aoc_9.prep_data(r'test/aoc9.txt')) == 127
+
+
+    def test_day9b(self):
+
+        assert aoc_9.day9b(aoc_9.prep_data(r'test/aoc9.txt')) == 62
